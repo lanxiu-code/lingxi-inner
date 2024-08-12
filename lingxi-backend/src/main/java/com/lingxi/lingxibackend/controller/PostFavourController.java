258de8bdcd3e8209c3afc52be79a08691da287bc
lx <3403138527@qq.com>
2024-08-12 17:17:53 +0800

    init

---
+++ lingxi-backend/src/main/java/com/lingxi/lingxibackend/controller/PostFavourController.java
@@ -0,0 +1,109 @@
+package com.lingxi.lingxibackend.controller;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.lingxi.lingxibackend.common.BaseResponse;
+import com.lingxi.lingxibackend.common.ErrorCode;
+import com.lingxi.lingxibackend.common.ResultUtils;
+import com.lingxi.lingxibackend.exception.BusinessException;
+import com.lingxi.lingxibackend.exception.ThrowUtils;
+import com.lingxi.lingxibackend.model.dto.post.PostQueryRequest;
+import com.lingxi.lingxibackend.model.dto.postfavour.PostFavourAddRequest;
+import com.lingxi.lingxibackend.model.dto.postfavour.PostFavourQueryRequest;
+import com.lingxi.lingxibackend.model.entity.Post;
+import com.lingxi.lingxibackend.model.entity.User;
+import com.lingxi.lingxibackend.model.vo.PostVO;
+import com.lingxi.lingxibackend.service.PostFavourService;
+import com.lingxi.lingxibackend.service.PostService;
+import com.lingxi.lingxibackend.service.UserService;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 帖子收藏接口
+ *
+ * 
+ * 
+ */
+@RestController
+@RequestMapping("/post_favour")
+@Slf4j
+public class PostFavourController {
+
+    @Resource
+    private PostFavourService postFavourService;
+
+    @Resource
+    private PostService postService;
+
+    @Resource
+    private UserService userService;
+
+    /**
+     * 收藏 / 取消收藏
+     *
+     * @param postFavourAddRequest
+     * @param request
+     * @return resultNum 收藏变化数
+     */
+    @PostMapping("/")
+    public BaseResponse<Integer> doPostFavour(@RequestBody PostFavourAddRequest postFavourAddRequest,
+            HttpServletRequest request) {
+        if (postFavourAddRequest == null || postFavourAddRequest.getPostId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        // 登录才能操作
+        final User loginUser = userService.getLoginUser(request);
+        long postId = postFavourAddRequest.getPostId();
+        int result = postFavourService.doPostFavour(postId, loginUser);
+        return ResultUtils.success(result);
+    }
+
+    /**
+     * 获取我收藏的帖子列表
+     *
+     * @param postQueryRequest
+     * @param request
+     */
+    @PostMapping("/my/list/page")
+    public BaseResponse<Page<PostVO>> listMyFavourPostByPage(@RequestBody PostQueryRequest postQueryRequest,
+            HttpServletRequest request) {
+        if (postQueryRequest == null) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        User loginUser = userService.getLoginUser(request);
+        long current = postQueryRequest.getCurrent();
+        long size = postQueryRequest.getPageSize();
+        // 限制爬虫
+        ThrowUtils.throwIf(size > 20, ErrorCode.PARAMS_ERROR);
+        Page<Post> postPage = postFavourService.listFavourPostByPage(new Page<>(current, size),
+                postService.getQueryWrapper(postQueryRequest), loginUser.getId());
+        return ResultUtils.success(postService.getPostVOPage(postPage, request));
+    }
+
+    /**
+     * 获取用户收藏的帖子列表
+     *
+     * @param postFavourQueryRequest
+     * @param request
+     */
+    @PostMapping("/list/page")
+    public BaseResponse<Page<PostVO>> listFavourPostByPage(@RequestBody PostFavourQueryRequest postFavourQueryRequest,
+            HttpServletRequest request) {
+        if (postFavourQueryRequest == null) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        long current = postFavourQueryRequest.getCurrent();
+        long size = postFavourQueryRequest.getPageSize();
+        Long userId = postFavourQueryRequest.getUserId();
+        // 限制爬虫
+        ThrowUtils.throwIf(size > 20 || userId == null, ErrorCode.PARAMS_ERROR);
+        Page<Post> postPage = postFavourService.listFavourPostByPage(new Page<>(current, size),
+                postService.getQueryWrapper(postFavourQueryRequest.getPostQueryRequest()), userId);
+        return ResultUtils.success(postService.getPostVOPage(postPage, request));
+    }
+}
---
+++ lingxi-backend/src/main/java/com/lingxi/lingxibackend/mapper/PostThumbMapper.java
@@ -0,0 +1,18 @@
+package com.lingxi.lingxibackend.mapper;
+
+import com.lingxi.lingxibackend.model.entity.PostThumb;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 帖子点赞数据库操作
+ *
+ * 
+ * 
+ */
+public interface PostThumbMapper extends BaseMapper<PostThumb> {
+
+}
+
+
+
+
